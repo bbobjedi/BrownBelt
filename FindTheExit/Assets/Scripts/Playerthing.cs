f6856dc89ad1dc2d070d9d532e1f08c14fadc83b
MasterHacker10 <141876965+MasterHacker10@users.noreply.github.com>
2024-01-10 18:29:39 -0500

    scsaxdadsad

--- FindTheExit/Assets/Scripts/Playerthing.cs
+++ FindTheExit/Assets/Scripts/Playerthing.cs
@@ -2,21 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class Playerthing : MonoBehaviour
 {
     public float speed = 5;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-        Vector3 destination = new Vector3(horizontal, 0, horizontal);
+        Vector3 destination = new Vector3(horizontal, 0, 1);
         transform.Translate(destination * speed * Time.deltaTime);
     }
 }
